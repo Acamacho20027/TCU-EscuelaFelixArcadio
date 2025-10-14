55c6c523252ae4bca886cd000b2c568e30bbc784
Acamacho2004 <andrescamachosanchez70@gmail.com>
2025-10-13 22:58:33 -0600

    Agregar archivos de proyecto.

---
+++ EscuelaFelixArcadio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EscuelaFelixArcadio.MvcApplication" Language="C#" %>
---
+++ EscuelaFelixArcadio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EscuelaFelixArcadio.Startup))]
+namespace EscuelaFelixArcadio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
